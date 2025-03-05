854b28a1f808a9d89d8d42d22bc99ecf5b4ebe59
dodeteddy <dodeteddydev@gmail.com>
2025-03-05 17:06:02 +0800

    feat(auth): Implement UI for the login page

---
+++ src/components/global/InputField.tsx
@@ -0,0 +1,66 @@
+import { Eye, EyeOff } from "lucide-react";
+import { InputHTMLAttributes, useState } from "react";
+
+type InputFieldProps = {
+  label?: string;
+  error?: string;
+  isPasswordField?: boolean;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const InputField = ({
+  label,
+  error,
+  isPasswordField,
+  ...props
+}: InputFieldProps) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col">
+      {label && (
+        <label
+          htmlFor={label?.toLowerCase()}
+          className={`text-sm ${error && "text-red-500"}`}
+        >
+          {label}
+        </label>
+      )}
+
+      <div className="relative w-full ">
+        <input
+          {...props}
+          type={isPasswordField && !showPassword ? "password" : ""}
+          id={label?.toLowerCase()}
+          className={`border h-10 px-2 rounded-lg w-full ${
+            error
+              ? "border-red-500 focus:outline-red-500"
+              : "focus:outline-primary"
+          }`}
+        />
+
+        {isPasswordField && (
+          <button
+            disabled={props.disabled}
+            type="button"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? (
+              <EyeOff
+                size={20}
+                color={error ? "red" : props.disabled ? "gray" : undefined}
+              />
+            ) : (
+              <Eye
+                size={20}
+                color={error ? "red" : props.disabled ? "gray" : undefined}
+              />
+            )}
+          </button>
+        )}
+      </div>
+
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+    </div>
+  );
+};
